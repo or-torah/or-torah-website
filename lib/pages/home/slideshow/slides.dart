177dfc42f96b24b4bc9c9029056ab0c9210cbf5d
deborah mizrahi <debimizfel@gmail.com>
2023-07-17 16:14:59 +0300

    slideshow_overlay

--- lib/pages/home/slideshow/slides.dart
+++ lib/pages/home/slideshow/slides.dart
@@ -1,26 +1,35 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:or_torah_website/pages/home/slideshow_overlay/slideshow_overlay.dart';
 
 class Slides extends StatelessWidget {
   final CarouselController controller;
-  const Slides({super.key, required this.controller});
+  final bool wide;
+
+  const Slides({super.key, required this.controller, this.wide = true});
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: MediaQuery.of(context).size.width * 0.88,
-      child: CarouselSlider(
-        carouselController: controller,
-        options: CarouselOptions(
-          autoPlay: true,
-          autoPlayInterval: const Duration(seconds: 8),
-          viewportFraction: 1,
-          height: double.infinity,
-        ),
-        items: [
-          Container(color: Colors.red),
-          Container(color: Colors.blue),
-          Container(color: Colors.green)
+      child: Stack(
+        alignment: AlignmentDirectional.bottomEnd,
+        children: [
+          CarouselSlider(
+            carouselController: controller,
+            options: CarouselOptions(
+              autoPlay: true,
+              autoPlayInterval: const Duration(seconds: 8),
+              viewportFraction: 1,
+              height: double.infinity,
+            ),
+            items: [
+              Container(color: Colors.red),
+              Container(color: Colors.blue),
+              Container(color: Colors.green)
+            ],
+          ),
+          SlideshowOverlay(wide: wide),
         ],
       ),
     );
---
+++ lib/pages/home/slideshow_overlay/slideshow_overlay.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SlideshowOverlay extends StatelessWidget {
+  final bool wide;
+
+  const SlideshowOverlay({super.key, this.wide = true});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      return FractionallySizedBox(
+        widthFactor: wide ? 0.4 : 1,
+        heightFactor: wide ? 1 : 0.4,
+        child: Container(
+          color: const Color.fromARGB(77, 245, 245, 245),
+        ),
+      );
+    });
+  }
+}
